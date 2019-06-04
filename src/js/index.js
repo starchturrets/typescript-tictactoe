3ebe4f93990c1b795822c62de862643cd1a52150
James Thaumiel <starchturrets@protonmail.com>
2019-06-04 14:08:32 +0400

    More testing

--- src/js/index.js
+++ src/js/index.js
@@ -116,7 +116,7 @@ function winner() {
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('sw.js')
+		.register('/sw.js')
 		.then((reg) => {
 			// registration worked
 			console.log('Registration succeeded. Scope is ' + reg.scope);
