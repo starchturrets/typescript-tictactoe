bea971702ca8b0695c9128bdfaa6205f783f39c4
James Thaumiel <starchturrets@protonmail.com>
2019-06-04 14:43:15 +0400

    test

--- sw.js
+++ sw.js
@@ -1,47 +1,62 @@
 //Thank God for Brad Traversy Media
-const cacheName = 'v2';
-const cacheAssets = [
-	//Stuff to cache
-	'/',
-	'index.html',
-	'sw.js',
-	'/manifest.json',
-	'/src/js/index.js',
-	'/src/style.css',
-	'/src/assets/apple-touch-icon.png',
-	'/src/assets/favicon-32x32.png',
-	'/src/assets/favicon-16x16.png',
-	'/src/assets/safari-pinned-tab.svg',
-	'/src/assets/favicon.ico',
-	'/src/assets/browserconfig.xml'
-];
+// const cacheName = 'v2';
+// const cacheAssets = [
+// 	//Stuff to cache
+// 	'/',
+// 	'index.html',
+// 	'sw.js',
+// 	'/manifest.json',
+// 	'/src/js/index.js',
+// 	'/src/style.css',
+// 	'/src/assets/apple-touch-icon.png',
+// 	'/src/assets/favicon-32x32.png',
+// 	'/src/assets/favicon-16x16.png',
+// 	'/src/assets/safari-pinned-tab.svg',
+// 	'/src/assets/favicon.ico',
+// 	'/src/assets/browserconfig.xml'
+// ];
+
 
 
 self.addEventListener('install', (event) => {
 	event.waitUntil(
-		caches.open(cacheName).then((cache) => {
-			return cache.addAll(cacheAssets);
+		caches.open('I hate life').then((cache) => {
+			return cache.addAll( [
+				//Stuff to cache
+				'/',
+				'index.html',
+				'sw.js',
+				'/manifest.json',
+				'/src/js/index.js',
+				'/src/style.css',
+				'/src/assets/apple-touch-icon.png',
+				'/src/assets/favicon-32x32.png',
+				'/src/assets/favicon-16x16.png',
+				'/src/assets/safari-pinned-tab.svg',
+				'/src/assets/favicon.ico',
+				'/src/assets/browserconfig.xml'
+			]);
 		})
 	);
 });
 
 // Call Activate Event
-self.addEventListener('activate', (e) => {
-	console.log('Service Worker: Activated');
-	// Remove unwanted caches
-	e.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cache) => {
-					if (cache !== cacheName) {
-						console.log('Service Worker: Clearing Old Cache');
-						return caches.delete(cache);
-					}
-				})
-			);
-		})
-	);
-});
+// self.addEventListener('activate', (e) => {
+// 	console.log('Service Worker: Activated');
+// 	// Remove unwanted caches
+// 	e.waitUntil(
+// 		caches.keys().then((cacheNames) => {
+// 			return Promise.all(
+// 				cacheNames.map((cache) => {
+// 					if (cache !== cacheName) {
+// 						console.log('Service Worker: Clearing Old Cache');
+// 						return caches.delete(cache);
+// 					}
+// 				})
+// 			);
+// 		})
+// 	);
+// });
 
 // Call Fetch Event
 self.addEventListener('fetch', (e) => {
