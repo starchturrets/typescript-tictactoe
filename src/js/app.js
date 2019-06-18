8745becd859666ce75c28d6ad6015e3373aee42e
James <37842965+starchturrets@users.noreply.github.com>
2019-06-18 12:15:06 +0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -120,3 +120,15 @@ const winningCombos = [
 ];
 
 decideLetters();
+ if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/sw.js')
+		.then((reg) => {
+			// registration worked
+			console.log('Registration succeeded. Scope is ' + reg.scope);
+		})
+		.catch((error) => {
+			// registration failed
+			console.log('Registration failed with ' + error);
+		});
+}
