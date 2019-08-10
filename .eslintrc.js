710c8b4f76998a5671028efb420a021eea6a2dad
James Thaumiel <starchturrets@protonmail.com>
2019-08-10 15:01:31 +0800

    Progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'prefer-destructuring': 'off',
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,81 @@
-class Game {
-  hook: Element;
+/* eslint-disable no-use-before-define */
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-  gameState: string[];
+const gameState: string[] = ['', '', '', '', '', '', '', '', ''];
 
-  constructor(hook: Element, gameState: string[]) {
-    this.hook = hook;
-    this.gameState = gameState;
-    this.gameState.forEach((item: string, index: number) => {
-      console.log(item);
-      const el: Element = hook;
-      el.children[index].textContent = item;
-    });
+class Player {
+  letter: string;
+
+  won: boolean;
+
+  constructor(letter: string, won: boolean) {
+    this.letter = letter;
+    this.won = won;
   }
 }
+class Game {
+  // Handles game stuff
 
-const gameState: string[] = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'X'];
+  stateArray: string[];
 
-const board: Element = document.querySelector('div.game-board');
-const game = new Game(board, gameState);
+  playerOne: any;
+
+  playerOneTurn: boolean = true;
+
+  playerTwo: any;
+
+  board: Element;
+
+  boardChildren: NodeListOf<Element>;
+
+  constructor(stateArray: string[], target: Element) {
+    this.stateArray = stateArray;
+    this.board = target;
+
+    this.boardChildren = target.querySelectorAll('div')!;
+  }
+
+  initialize(userLetter: string, playerLetter: string) {
+    this.playerOne = new Player(userLetter, false);
+    this.playerTwo = new Player(playerLetter, false);
+    this.enableListeners();
+  }
+
+  updateState = (index: number, letter: string) => {
+    this.stateArray[index] = letter;
+    this.updateBoard(this.stateArray);
+  };
+
+  updateBoard = (state: string[]) => {
+    state.forEach((letter: string, index: number) => {
+      if (this.boardChildren[index].textContent === '') {
+        this.boardChildren[index].textContent = letter;
+      }
+    });
+  };
+
+  enableListeners = () => {
+    this.boardChildren.forEach((el: Element) => {
+      el.addEventListener('click', this.handle);
+    });
+  };
+
+  handle = (ev: Event) => {
+    if (ev.target instanceof HTMLElement) {
+      const index: number = Number(ev.target!.dataset!.index!);
+      if (this.playerOneTurn) {
+        board.updateState(index, this.playerOne.letter);
+      } else {
+        board.updateState(index, this.playerTwo.letter);
+      }
+      this.updateBoard(board.stateArray);
+    }
+    this.playerOneTurn = !this.playerOneTurn;
+  };
+
+  turn = () => {};
+}
+
+const board = new Game(gameState, $('div.game-board')!);
+board.initialize('X', 'O');
